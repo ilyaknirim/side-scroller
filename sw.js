35f5cbd15ac88f8d08bd00e6a66b536c1fbb4df7
ilyaknirim <ilya.knirim@gmail.com>
2025-10-30 13:09:37 +0300

    `Update index.html and sw.js to remove preload for audio_manager.js and add it to precache list`

--- sw.js
+++ sw.js
@@ -3,6 +3,21 @@ const PRECACHE = [
  './',
  './index.html',
  './styles.css',
+ './src/ui/app.js',
+ './src/systems/audio_manager.js',
+ './src/systems/meditations.js',
+ './src/systems/gallery.js',
+ './src/systems/proc_gen.js',
+ './src/systems/leaderboard.js',
+ './src/systems/particles.js',
+ './src/modes/demo_games/flappy_demo.js',
+ './src/modes/demo_games/doodle_demo.js',
+ './src/modes/demo_games/snake_demo.js',
+ './src/modes/demo_games/dino_demo.js',
+ './src/modes/demo_games/breakout_demo.js',
+ './src/modes/demo_games/2048_demo.js',
+ './manifest.json',
+ './favicon.svg',
 ];
 self.addEventListener('install', (event) => {
  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE)));
